b712ac04bacc7716772cfd7b1ba92486b90f3a64
𝐒𝐀𝐇𝐈𝐋-𝐌𝐃💀🚩 <itxme6616@gmail.com>
2026-07-25 03:14:16 +0500

    Update author name in dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -1,4 +1,4 @@
-// ERFAN-MD
+// 𝐒𝐀𝐇𝐈𝐋-𝐌𝐃💀🚩
 import { fileURLToPath } from 'url';
 import path from 'path';
 import axios from 'axios';
@@ -58,7 +58,7 @@ cmd({
         if (urls.length < 1) return reply('✧ No images found.')
         
         const medias = urls.slice(0, 10).map(url => ({ image: { url } }))
-        const caption = `> SAHIL-MD`
+        const caption = `> 𝐒𝐀𝐇𝐈𝐋-𝐌𝐃💀🚩`
         
         for (let media of medias) {
             await conn.sendMessage(from, media, { quoted: m })
